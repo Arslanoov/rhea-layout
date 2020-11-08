7e55b1dd09e67e9384eeeaa2cf6fc79337363b75
Arslanoov <rasularslanoov@gmail.com>
2020-11-08 13:03:34 +0500

    Added babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
+const fs = require('fs');
+
+const PAGES_DIR = `src/pug/views/`;
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
 
 module.exports = {
     mode: "development",
 
     resolve: {
-        extensions: [".js", ".css", ".scss", ".sass"]
+        extensions: [".pug", ".js", ".css", ".scss", ".sass"]
     },
 
     entry: [
@@ -16,36 +20,38 @@ module.exports = {
     ],
 
     plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/html/views/index.html'
-        }),
+        ...PAGES.map(page => new HtmlWebpackPlugin({
+            template: `${PAGES_DIR}/${page}`,
+            filename: `./${page.replace(/\.pug/,'.html')}`
+        })),
         new MiniCssExtractPlugin({
             filename: 'main.css'
         }),
-        /*new CopyWebpackPlugin([{
+        new CopyWebpackPlugin({
             patterns: [
-                {
-                    from: './src/fonts',
-                    to: './fonts'
-                },
-                {
-                    from: './src/favicon',
-                    to: './favicon'
-                },
                 {
                     from: './src/img',
                     to: './img'
-                },
-                {
-                    from: './src/uploads',
-                    to: './uploads'
                 }
             ]
-        }])*/
+        })
     ],
 
     module: {
         rules: [
+            // Loading pug
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader'
+            },
+
+            // Loading js
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+
             // Loading CSS
             {
                 test: /\.(css)$/,
